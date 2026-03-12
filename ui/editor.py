4d931d9e04b330a6f789a500dcaa989f09532b3e
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2026-03-12 09:50:31 +0000

    Update editor.py

--- ui/editor.py
+++ ui/editor.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+
+def editable_dataframe(df):
+
+    edited = st.data_editor(
+        df,
+        use_container_width=True,
+        num_rows="fixed"
+    )
+
+    return edited
