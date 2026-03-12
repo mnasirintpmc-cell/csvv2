ff2dcf8cbda25a61cb6dd7ac6e5e4f944f805e23
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2026-03-12 08:15:56 +0000

    Update csv_reader.py

--- io/csv_reader.py
+++ io/csv_reader.py
@@ -1,1 +1,17 @@
+# io/csv_reader.py
 
+import pandas as pd
+
+def load_csv(file):
+
+    encodings = ["utf-8","latin-1","cp1252"]
+
+    for enc in encodings:
+
+        try:
+            return pd.read_csv(file, delimiter=";", encoding=enc)
+
+        except:
+            continue
+
+    raise Exception("Unable to read CSV file")
