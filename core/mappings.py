3f1f9948665f04fb5d11d9b307afb21051658f64
mnasirintpmc-cell <mnasirintpmc@gmail.com>
2026-03-12 08:14:42 +0000

    Update mappings.py

--- core/mappings.py
+++ core/mappings.py
@@ -1,1 +1,21 @@
+# core/mappings.py
 
+def machine_to_technician(df, mapping):
+
+    tech = df.rename(columns=mapping)
+
+    tech.insert(0, "Step", range(1, len(tech)+1))
+
+    if "Notes" not in tech.columns:
+        tech["Notes"] = ""
+
+    return tech
+
+
+def technician_to_machine(df, reverse_mapping):
+
+    machine = df.rename(columns=reverse_mapping)
+
+    machine = machine.drop(columns=["Step","Notes"], errors="ignore")
+
+    return machine
